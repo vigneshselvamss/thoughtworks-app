cf0f981c7b4a73740c127ffdb9f6f24715908952
vignesh.selvam <vignesh>
2020-10-21 12:16:01 +0530

    updating desscription in playbook

--- scripts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo su
-apt-add-repository ppa:ansible/ansible -y
-apt-get update -y
-apt-get install ansible -y
-apt-get install git -y
-git clone https://github.com/vigneshselvamss/thoughtworks-app
-cd  thoughtworks-app
-ansible-playbook mediawiki-playbook.yml
